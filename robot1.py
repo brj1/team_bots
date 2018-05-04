35ecf6f10a6b02875f8be878d723c873d66781c7
Brad Jones <brj1@users.noreply.github.com>
2018-05-04 10:55:40 -0700

    Update robot1.py

--- robot1.py
+++ robot1.py
@@ -5,6 +5,7 @@ TARGET = (5, 5)
 
 class Robot:
     def act(self, game):
+        print('My robot')
         if self.location == TARGET:
             return ['guard']
         return ['move', rg.toward(self.location, TARGET)]
